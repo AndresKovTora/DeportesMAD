a988ea0ba2d31998e3ab507af7b95562153ed5a0
Andrés Kovács Torá <a.ktora@alumnos.upm.es>
2026-05-03 14:22:00 +0200

    Actualizado include/README.md
    Es el archivo que añade las descripciones que podéis ver en GitHub.

--- include/README.md
+++ include/README.md
@@ -3,3 +3,18 @@ Aquí van todos los archivos *Header*, es decir, los archivos tipo `librería.h`
 ```c
 #include "include/librería.h"
 ```
+## Librería lector
+### Estructuras
+#### Linea
+La estructura actividad contiene la información relativa a una actividad que ocurre a una hora, en un centro, con un tipo, etc, es decir, a cada una de las líneas del CSV que se proporciona [aquí](../data/dataset.csv). Es la base de todo el programa, todos los datos estarán en un array de estructuras **línea**.
+
+### Funciones
+```c
+linea *csv_a_actividades(char *ruta_al_CSV, u_int *tamano);
+```
+Encargada de tomar el [archivo CSV](../data/dataset.csv) y convertirlo al array de estructuras de tipo línea mencionado en [[#Linea]].
+
+```c
+int contar_lineas(FILE *archivo);
+```
+Función interna a `lector.c` usada por `csv_a_actividades`, que cuenta las líneas del [CSV](../data/dataset.csv) para saber el tamaño en memoria que habrá que reservar.
--- include/lector.h
+++ include/lector.h
@@ -21,24 +21,10 @@
 
 /* Definición de estructuras */
 
-// Fecha.
-// Información temporal relativa a una actividad concreta.
-typedef struct
-{
-  int horaInicio;
-  int horaFin;
-  enum diaSemana {
-    lunes, martes, miercoles, jueves, viernes, sabado, domingo
-  };
-  int diaMes;
-  int mes;
-  int ano;
-} fecha;
-
-// Actividad.
-// Información de una sola actividad del polideportivo. Cada índice de los arrays siguientes
-// hace referencia a una instancia de la actividad, p.ej, la actividad "natación" se da 50 veces en el
-// polideportivo X, pues cada uno de los índices de 0 a 49 representa una de las veces que se da natación.
+// Línea.
+// Información relativa a una instancia de una actividad, es decir, una actividad, a una hora, en un
+// centro, con una modalidad, etc. La información contenida por una estructura de tipo linea es
+// equivalente a la escrita en una línea del CSV en data/dataset.csv, de ahí el nombre.
 typedef struct
 {
   int  anio;
@@ -57,9 +43,6 @@ typedef struct
 } linea;
 
 /* Definición de funciones */
-
-// Saltar la primera línea del archivo, que contiene los títulos.
-void saltar_primera_linea(FILE *archivo);
 
 // Contar número de líneas del archivo.
 int contar_lineas(FILE *archivo);
