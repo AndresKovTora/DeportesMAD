216380c49fe00699d77d7e67248942fab446fa50
Andrés Kovács Torá <a.ktora@alumnos.upm.es>
2026-05-05 15:26:55 +0200

    Actualizado .gitignore
    Se estaba guardando la carpeta out/ de Yassine, *se supone* que ya está
    quitada.

---
+++ build/CMakeFiles/DeportesMAD-bin.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeportesMAD-bin.dir/link.d"
+  "CMakeFiles/DeportesMAD-bin.dir/src/lector.c.o"
+  "CMakeFiles/DeportesMAD-bin.dir/src/lector.c.o.d"
+  "CMakeFiles/DeportesMAD-bin.dir/src/main.c.o"
+  "CMakeFiles/DeportesMAD-bin.dir/src/main.c.o.d"
+  "DeportesMAD-bin"
+  "DeportesMAD-bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DeportesMAD-bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
