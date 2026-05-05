3476a495931d87a66d91be619f5f3016b6291ffa
Andrés Kovács Torá <a.ktora@alumnos.upm.es>
2026-05-05 15:29:02 +0200

    Ahora sí, joder
    Que no se había quitado mi carpeta build/

--- build/include/config.h
+++
@@ -1,8 +0,0 @@
-// Este es un archivo de configuración para CMake, para que no haya problemas con la ruta del dataset.csv.
-
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define RUTA_DATASET "/home/adrw216/UPM/2-cuatri/informatica/proyecto/app/data/dataset.csv"
-
-#endif
