a23b9f0692e2bd5c852a8ae4e61e9481d66b493b
YassinetripleT <yassner234@gmail.com>
2026-05-06 19:13:37 +0200

    vale esta es la definitiva para descrpición ver el último push que he hecho gracias

--- src/main.c
+++ src/main.c
@@ -19,14 +19,14 @@ int main()
     return 1;
   }
 
-  // Prueba de lista_centros (mía)
+  // Prueba de lista_centros 
   printf("\n=== Lista de centros unicos ===\n");
   lista_centros(datos, tamano);
 
-  // Prueba de actividad concreta (de Andres)
+  // Prueba de actividad concreta
   printf("\nActividad de la linea 10: %s\n", datos[10].actividad_base);
 
-  // Prueba de elegir_centro (de Andres)
+  // Prueba de elegir_centro 
   char *centro_elegido = elegir_centro(datos, tamano);
   printf("\nHas elegido: %s\n", centro_elegido);
 
